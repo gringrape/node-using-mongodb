bed58322d163a7188a321578503409ed17d4bb3c
gringrape <gringrape200@gmail.com>
2020-06-03 17:16:52 +0900

    Completed module

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = 4000;
 
 // connection
-const connetion = mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
--- push.bat
+++ push.bat
@@ -1,4 +1,5 @@
 @echo off
-git add . && git commit -m 'Completed module'
+git add .
+git commit -m "Completed module"
 git push origin master
 pause
